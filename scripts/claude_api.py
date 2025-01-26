cd34dc70080929e6edfa3efdf37e9fcd5e3c1ec9
Hit the Code Labs <145279880+hitthecodelabs@users.noreply.github.com>
2025-01-25 20:54:07 -0500

    Update claude_api.py

--- scripts/claude_api.py
+++ scripts/claude_api.py
@@ -5,12 +5,14 @@ client = anthropic.Anthropic(
     api_key="my_api_key",
 )
 
+prompt = 'Hi'
+
 message = client.messages.create(
     # model="claude-3-5-sonnet-20240620",
     # model="claude-3-5-haiku-20241022",
     model="claude-3-5-sonnet-20241022",
     max_tokens=1000,
     temperature=0,
-    messages=[]
+    messages=[prompt]
 )
 print(message.content)
